d4a55b9eb2553846e957476e7c77241a14dbd2b4
paulius <paulius_puodziunas@fmail.co.uk>
2015-12-22 11:52:39 +0200

    simple test

---
+++ Assets/CloudTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CloudTest  {
+
+	public static void StartMain () {
+
+		Debug.LogWarning(" - - - - - - -RANDOM SPAM -- START - - - - - -- ");
+
+		Debug.Log(" 12345678 --- - 87654321");
+
+		Debug.LogWarning(" - - - - - - -RANDOM SPAM -- OVER- - - - - -- ");
+	}
+}
