fe47cb93ad39047fd182af515119382a6cf682f8
paulius <paulius_puodziunas@fmail.co.uk>
2015-12-22 12:42:21 +0200

    added testRunner

--- Assets/CloudTest.cs
+++ Assets/CloudTest.cs
@@ -5,12 +5,10 @@ public static class CloudTest  {
 
 	public static void StartMain () {
 
-		Debug.LogWarning(" - - - - - - -RANDOM SPAM -- START - - - - - -- ");
 
-		Debug.Assert( 3f > 1f);
-
-		throw new System.Exception("random spam EXCEPTION!");
-
-		Debug.LogWarning(" - - - - - - -RANDOM SPAM -- OVER- - - - - -- ");
 	}
+
+
 }
+
+
---
+++ Assets/Editor/TestRunner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRunner : MonoBehaviour {
+
+	// Use this for initialization
+	public static void Execute () {
+
+		var testRunner = new GameObject().AddComponent<TestRunner>();
+
+		testRunner.UpdatePlayerSettings();
+		testRunner.ThrownAnException();
+
+		Destroy(testRunner.gameObject);
+	}
+
+	public void Start(){
+		Debug.Log(" --- Starting TestRunner !");
+	}
+
+	public void UpdatePlayerSettings(){
+
+		Debug.Log(" ---  TestRunner updating player settings:");
+
+		//increment build number
+		UnityEditor.PlayerSettings.iOS.buildNumber = "11";
+
+		UnityEditor.PlayerSettings.bundleIdentifier = "random.noCompany.someApp";
+		UnityEditor.PlayerSettings.iOS.applicationDisplayName = "someApp";
+
+		UnityEngine.Assertions.Assert.IsTrue( UnityEditor.PlayerSettings.iOS.buildNumber == "11");
+		UnityEngine.Assertions.Assert.IsTrue( UnityEditor.PlayerSettings.iOS.applicationDisplayName == "someApp");
+		UnityEngine.Assertions.Assert.IsTrue( UnityEditor.PlayerSettings.bundleIdentifier == "random.noCompany.someApp");
+
+
+		Debug.Log(
+			" ---  \n buildNumber" + UnityEditor.PlayerSettings.iOS.buildNumber +
+			" ---  \n bundleIdentifier" + UnityEditor.PlayerSettings.bundleIdentifier +
+			" ---  \n applicationDisplayName" + UnityEditor.PlayerSettings.iOS.applicationDisplayName
+		);
+	
+	}
+
+	public void ThrownAnException(){
+
+		Debug.Log(" ---  TestRunner is throwing an Exception!");
+		throw new System.Exception("Throwing a standard Exception !");
+	}
+
+	void OnDestroy() {
+		Debug.Log(" ---  TestRunner destroyed!");
+    }
+
+}
