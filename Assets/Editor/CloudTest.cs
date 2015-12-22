beb6d5141b738e6826fc62e7295e5faf6834ac22
paulius <paulius_puodziunas@fmail.co.uk>
2015-12-22 12:59:50 +0200

    pre post proc

--- Assets/Editor/CloudTest.cs
+++ Assets/Editor/CloudTest.cs
@@ -12,6 +12,20 @@ public static class CloudTest  {
 	}
 
 
+	public static void PreExportUpdateAppData(){
+
+		Debug.LogWarning( " Pre-Export : UpdateAppData \n buildNumber = 13 \n applicationDisplayName = someApp_RandomName_111");
+
+		UnityEditor.PlayerSettings.iOS.buildNumber = "13";
+		UnityEditor.PlayerSettings.iOS.applicationDisplayName = "someApp_RandomName_111");
+	}
+
+	public static void PostExportDebugLog(){
+
+		Debug.LogWarning( " Post-Export : function called");
+
+	}
+
 }
 
 
